fb9628de0af1477a2a9e256bab588460e2188a24
gyanish0 <srivastavagyanish@gmail.com>
2023-03-28 16:57:16 +0530

    navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
       <body>
         <main className="bg-gray-100 min-h-screen w-screen">
           <main className="max-w-screen-2xl m-auto bg-white">
-            <Navbar />
+            {/* <Navbar /> */}
             {children}
           </main>
         </main>
