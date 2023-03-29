6c0d7b075d8bb117c331fdcbc71736e61689f8b9
gyanish0 <srivastavagyanish@gmail.com>
2023-03-29 14:16:12 +0530

    add images for restaurant

--- app/restaurant/[slug]/components/Images.tsx
+++ app/restaurant/[slug]/components/Images.tsx
@@ -2,15 +2,16 @@ function Images({ images }: { images: string[] }) {
     return (
         <div>
             <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">
-                5 photos
+                {images.length} photo{images.length > 1 ? 's' : ''}
             </h1>
             <div className="flex flex-wrap">
-                {images.map((img) => {
+                {images.map((img, i) => {
                     return (
                         <img
+                            key={i}
                             className="w-56 h-44 mr-1 mb-1"
-                            src="https://resizer.otstatic.com/v2/photos/xlarge/3/41701449.jpg"
-                            alt=""
+                            src={img}
+                            alt="test"
                         />
                     )
                 })}
